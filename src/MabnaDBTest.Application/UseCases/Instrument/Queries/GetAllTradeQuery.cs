2550bfce75f29e12985899a80f8acded8b6c85ed
Yasin Asadnezhad <yasin.asadnezhad@msn.com>
2023-07-18 12:18:39 +0330

    changed on queries

--- src/MabnaDBTest.Application/UseCases/Instrument/Queries/GetAllTradeQuery.cs
+++ src/MabnaDBTest.Application/UseCases/Instrument/Queries/GetAllTradeQuery.cs
@@ -12,6 +12,9 @@ using Microsoft.EntityFrameworkCore;
 using Dapper;
 using Microsoft.Data.SqlClient;
 using System.Text;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Diagnostics.Metrics;
+using System.Xml.Linq;
 
 namespace MabnaDBTest.Application.UseCases.Instrument.Queries;
 
@@ -27,6 +30,21 @@ public class GetAllTradeQuery : IRequest<CustomResponseDto<IEnumerable<GetAllTra
     public async Task<CustomResponseDto<IEnumerable<GetAllTradeResponse>>> Handle(
         CancellationToken cancellationToken)
     {
+        /*
+         * USE [Lerning]
+ 
+        IF Not Exists(Select 1 FRom  sys.indexes Where Name = N'IX_NonClusteredIndex_Trade_InstrumentId')
+Begin
+    CREATE NONCLUSTERED INDEX IX_NonClusteredIndex_Trade_InstrumentId ON[dbo].[Trade]
+        (
+
+
+        [InstrumentId] ASC
+	)
+	INCLUDE([Id],[DateEn],[Open],[High],[Low],[Close]) WITH(PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON[PRIMARY]
+End
+
+         */
         StringBuilder SqlQuery = new StringBuilder(" SELECT instruments.Name, trades.*");
         SqlQuery.AppendLine(" FROM dbo.Instruments instruments");
         SqlQuery.AppendLine(" JOIN dbo.Trades trades ON instruments.Id = trades.InstrumentId");
