cc8da2f9c894ee158172c6a71c5c9b99eee73a5e
Yasin Asadnezhad <yasin.asadnezhad@msn.com>
2023-07-19 13:37:18 +0330

    Added UseInMemoryDatabase

--- src/MabnaDBTest.IoC/APIConfiguration.cs
+++ src/MabnaDBTest.IoC/APIConfiguration.cs
@@ -16,11 +16,14 @@ public static class APIConfiguration
 {
     public static void Register(this IServiceCollection services, IConfiguration configuration)
     {
-        services.AddDbContext<MAIN_MabnaDbContext>(options => options.UseSqlServer(configuration["ApplicationOptions:MAIN_MabnaDBConnectionString"]));
-        services.AddDbContext<READ_MabnaDbContext>(options => options.UseSqlServer(configuration["ApplicationOptions:READ_MabnaDBConnectionString"]));
-        services.AddDbContext<WRITE_MabnaDbContext>(options => options.UseSqlServer(configuration["ApplicationOptions:WRTIE_MabnaDBConnectionString"]));
-        //builder.Services.AddDbContext<MyDataBase>(options => options.UseInMemoryDatabase("MyDB"));           
-        //services.AddMediatR(Assembly.GetExecutingAssembly());
+        //services.AddDbContext<MAIN_MabnaDbContext>(options => options.UseSqlServer(configuration["ApplicationOptions:MAIN_MabnaDBConnectionString"]));
+        //services.AddDbContext<READ_MabnaDbContext>(options => options.UseSqlServer(configuration["ApplicationOptions:READ_MabnaDBConnectionString"]));
+        //services.AddDbContext<>(options => options.UseSqlServer(configuration["ApplicationOptions:WRTIE_MabnaDBConnectionString"]));
+
+        services.AddDbContext<MAIN_MabnaDbContext>(options => options.UseInMemoryDatabase("MabnaDbContext"));
+        services.AddDbContext<READ_MabnaDbContext>(options => options.UseInMemoryDatabase("MabnaDbContext"));
+        services.AddDbContext<WRITE_MabnaDbContext>(options => options.UseInMemoryDatabase("MabnaDbContext")); 
+ 
         //services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
         services.AddScoped<IUnitOfWork, UnitOfWork>();
         services.AddScoped<CoreDBContextInjection>();
